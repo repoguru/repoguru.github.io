b9cf0b899428de8f28bfe330123e09573c6c1e34
Khaled Badran <Badrankhaled97@gmail.com>
2019-08-08 10:43:56 -0400

    Update index.js

--- MSRBot/js/index.js
+++ MSRBot/js/index.js
@@ -157,7 +157,7 @@ $( "#welcome-button" ).click(function() {
   }
 });
 
-const url = "https://f651a3e2.ngrok.io/oz";
+const url = "https://30b6ea41.ngrok.io/oz";
 
 callServer = function(enteredText) {
         var cor = null; // cor stands alr Cross-Origin request
